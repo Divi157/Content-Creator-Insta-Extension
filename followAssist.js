c83e5372ebba529d4fb7d98dbff5cb3165bddb0d
Divi157 <divyasaini6798@gmail.com>
2024-05-23 03:12:25 -0700

    Better CX Version 2.0.1

--- followAssist.js
+++ followAssist.js
@@ -6,19 +6,28 @@
     const classNames = {
         postPopup : {
             elementType: 'postPopup',
-            elementClassName : 'x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x1n2onr6 x1plvlek xryxfnj x1c4vz4f x2lah0s x1q0g3np xqjyukv x1qjc9v5 x1oa3qoh xl56j7k',
+            elementClassName : 'xb88tzc xw2csxc x1odjw0f x5fp0pe x1qjc9v5 xjbqb8w x1lcm9me x1yr5g0i xrt01vj x10y3i5r xr1yuqi xkrivgy x4ii5y1 x1gryazu x15h9jz8 x47corl xh8yej3 xir0mxb x1juhsu6',
             actionElementClassName : 'x78zum5 x1q0g3np xwib8y2 x1yrsyyn x1xp8e9x x13fuv20 x178xt8z xdj266r x11i5rnm xat24cr x1mh8g0r xo1ph6p x1pi30zi x1swvt13',
             actionButtonClassName : 'postActionButton',
-            speedOptionsContainerClassName : 'postSpeedOptionsContainer',
+            speedOptionsContainerClassName : 'postSpeedOptionsContainer popover',
             speedOptionClassName : 'postSpeedOption',
         },
 
         postPage : {
             elementType: 'postPage',
             elementClassName : 'x6s0dn4 x78zum5 xdt5ytf xdj266r xkrivgy xat24cr x1gryazu x1n2onr6 xh8yej3',
-            actionElementClassName : 'x1xp8e9x x13fuv20 x178xt8z x9f619 x1yrsyyn x1pi30zi x10b6aqq x1swvt13 xh8yej3',
+            actionElementClassName : 'x1xp8e9x x13fuv20 x178xt8z x9f619 x1yrsyyn x1pi30zi x10b6aqq x1swvt13 xh8yej3 > x6s0dn4 xrvj5dj x1o61qjw > x78zum5',
             actionButtonClassName : 'postActionButton',
-            speedOptionsContainerClassName : 'postSpeedOptionsContainer',
+            speedOptionsContainerClassName : 'postSpeedOptionsContainer popover',
+            speedOptionClassName : 'postSpeedOption',
+        },
+
+        reelPage : {
+            elementType: 'reelPage',
+            elementClassName : 'x6s0dn4 x78zum5 xdt5ytf xdj266r xkrivgy xat24cr x1gryazu x1n2onr6 xh8yej3',
+            actionElementClassName : 'x1xp8e9x x13fuv20 x178xt8z x9f619 x1yrsyyn x1pi30zi x10b6aqq x1swvt13 xh8yej3 > x6s0dn4 xrvj5dj x1o61qjw > x78zum5',
+            actionButtonClassName : 'postActionButton',
+            speedOptionsContainerClassName : 'postSpeedOptionsContainer popover',
             speedOptionClassName : 'postSpeedOption',
         },
 
@@ -31,46 +40,54 @@
             // element class for action tab inside the reel element
             actionElementClassName : 'x9f619 xjbqb8w x78zum5 x168nmei x13lgxp2 x5pf9jr xo71vjh x12nagc x1uhb9sk x1plvlek xryxfnj x1c4vz4f x2lah0s xdt5ytf xqjyukv x6s0dn4 x1oa3qoh x13a6bvl x16n37ib x1247r65',
             actionButtonClassName : 'html-div xe8uvvx xdj266r x11i5rnm x1mh8g0r xexx8yu x4uap5 x18d9i69 xkhd6sd x6s0dn4 x78zum5 x1chd833',
-            speedOptionsContainerClassName : 'reelSpeedOptionsContainer',
+            speedOptionsContainerClassName : 'reelSpeedOptionsContainer popover',
             speedOptionClassName : 'reelSpeedOption',
         },
 
     }
 
-    instaLinkHandler(window.location.href);
+    instaLinkHandler();
+    registerLinkClickEvents();
 
-    // This function checks for insta link that's active and runs corresponding script functions and register link click events on the active page.
+    // This function checks for insta link that's active and runs corresponding script functions.
     function instaLinkHandler(href) {
         setTimeout(function() {
+            const href = window.location.href;
+
             if (href.indexOf('/reels/') !== -1) {
                 runReelsPageScriptFunctions();
             }
 
-            if (href.indexOf('/p') !== -1) {
+            if (href.indexOf('/p/') !== -1) {
                 runPostPageScriptFunctions();
             }
 
-            registerLinkClickEvents();
-        }, 2000);
+            if (href.indexOf('/reel/') !== -1) {
+                runPostPageScriptFunctions();
+            }
+        }, 1000);
     }
 
-    // This function gets all the links on the active page and attaches click event handlers.
+    // This function attaches click event handlers on document. Required as insta is one page application.
     function registerLinkClickEvents() {
-        const links = document.getElementsByTagName('a');
-        for (var i=0; i<links.length; i++) {
-            const link = links[i];
-            link.addEventListener('click', function(event) {
-                instaLinkHandler(link.getAttribute('href'));
-            });
-        }
+        document.addEventListener("click", function(event) {
+            var clickedElement = event.target;
+            const classesToNotMonitor = ['mirrorButton', 'speedButton', 'postSpeedOption', 'reelSpeedOption', 'speedIconClicked', 'speedIcon', 'flipIcon', 'flipIconClicked', 'popover'];
+            if (!classesToNotMonitor.includes(clickedElement.classList[clickedElement.classList.length - 1])) {
+                instaLinkHandler();
+            }
+        }, true);
     }
 
     // This function adds content creator helper action button on post page
     function runPostPageScriptFunctions() {
+        const isPostPopup = (document.getElementsByClassName(classNames.postPopup.elementClassName).length !== 0);
 
-        const postElement = document.getElementsByClassName(classNames.postPage.elementClassName)[0];
+        const classNamesForElementType = isPostPopup ? classNames.postPopup : classNames.postPage;
 
-        addContentCreatorHelperElementsToGivenReelOrPost(postElement, classNames.postPage.elementType);
+        const postElement = document.getElementsByClassName(classNamesForElementType.elementClassName)[0];
+
+        addContentCreatorHelperElementsToGivenReelOrPost(postElement, classNamesForElementType.elementType);
     }
 
     // This function adds content creator helper action buttons on reels page
@@ -122,12 +139,15 @@
         const mirrorButtonElement = document.createElement('div');
         mirrorButtonElement.className = classNames[elementType].actionButtonClassName;
         mirrorButtonElement.classList.add('mirrorButton');
-        mirrorButtonElement.textContent = 'Flip';
+
+        const flipIconImage = createImageElement('flipIcon', 'Flip'); 
+
+        mirrorButtonElement.appendChild(flipIconImage);
         mirrorButtonElement.onclick = function() {
             mirrorVideo(mirrorButtonElement, reelOrPostElement.getElementsByTagName('video')[0]);
         };
 
-        const reelOrPostActionElement = reelOrPostElement.getElementsByClassName(classNames[elementType].actionElementClassName)[0];
+        const reelOrPostActionElement = getElementsByClassHierarchy(reelOrPostElement, classNames[elementType].actionElementClassName)[0];
         reelOrPostActionElement.insertBefore(mirrorButtonElement, reelOrPostActionElement.firstChild);
         return mirrorButtonElement;
     }
@@ -146,14 +166,36 @@
         const speedOptionsContainerElement = createSpeedOptionsElements(reelOrPostElement, speedButtonElement, elementType);
         const speedButtonInnerTextElement = createSpeedButtonInnerTextElement(speedButtonElement, speedOptionsContainerElement);
 
-        const reelOrPostActionElement = reelOrPostElement.getElementsByClassName(classNames[elementType].actionElementClassName)[0];
+        const reelOrPostActionElement = getElementsByClassHierarchy(reelOrPostElement, classNames[elementType].actionElementClassName)[0];
         reelOrPostActionElement.insertBefore(speedButtonElement, reelOrPostActionElement.firstChild);
         return speedButtonElement;
     }
 
+    function getElementsByClassHierarchy(rootElement, classNamesHierarchy) {
+        const classNamesHierarchySplit =  classNamesHierarchy.split('>');
+        var currentElement = rootElement;
+        if (classNamesHierarchySplit.length === 1) {
+            return currentElement.getElementsByClassName(classNamesHierarchySplit[0]);
+        }
+
+        for (var i = 0; i < classNamesHierarchySplit.length; i++) {
+            const currentHierarchyClassName = classNamesHierarchySplit[i].trim();
+            const elementsWithCurrentHierarchyClassName = currentElement.getElementsByClassName(currentHierarchyClassName);
+
+            if (elementsWithCurrentHierarchyClassName.length === 0) {
+                return elementsWithCurrentHierarchyClassName;
+            }
+
+            currentElement = elementsWithCurrentHierarchyClassName[0];
+        }
+
+        return [currentElement];
+    }
+
     function createSpeedButtonInnerTextElement(speedButtonElement, speedOptionsContainerElement) {
         const speedButtonInnerTextElement = document.createElement('span');
-        speedButtonInnerTextElement.textContent = 'Speed';
+        const speedIconImage = createImageElement('speedIcon', 'Speed'); 
+        speedButtonInnerTextElement.appendChild(speedIconImage);
         speedButtonInnerTextElement.onclick = function() {
             toggleElement(speedOptionsContainerElement);
             toggleElement(speedButtonInnerTextElement);
@@ -194,6 +236,14 @@
             const speedButtonInnerTextElement = speedButtonElement.getElementsByTagName('span')[0];
             toggleElement(speedButtonInnerTextElement);
             changeSpeed(reelOrPostElement.getElementsByTagName('video')[0], speed);
+
+            if (speed !== 1) {
+                speedButtonInnerTextElement.removeChild(speedButtonInnerTextElement.getElementsByTagName('img')[0]); 
+                speedButtonInnerTextElement.appendChild(createImageElement('speedIconClicked', 'Speed'));
+            } else {
+                speedButtonInnerTextElement.removeChild(speedButtonInnerTextElement.getElementsByTagName('img')[0]); 
+                speedButtonInnerTextElement.appendChild(createImageElement('speedIcon', 'Speed'));
+            }
         };
 
         speedOptionsContainerElement.appendChild(speedOptionbutton);
@@ -206,7 +256,12 @@
         const currentTransform = computedStyle.getPropertyValue('transform');
         const isFlipped = currentTransform.includes('matrix(-1, 0, 0, 1, 0, 0)'); // checking for scaleX(-1)
 
-        mirrorButtonElement.textContent = isFlipped ? 'Flip' : 'UnFlip';
+        mirrorButtonElement.removeChild(mirrorButtonElement.getElementsByTagName('img')[0]); 
+        if (isFlipped) {
+            mirrorButtonElement.appendChild(createImageElement('flipIcon', 'Flip')); 
+        } else {
+            mirrorButtonElement.appendChild(createImageElement('flipIconClicked', 'UnFlip'));
+        }
 
         const newTransform = isFlipped ? 'scaleX(1)' : 'scaleX(-1)';
         videoElement.style.transform = newTransform;
@@ -219,5 +274,15 @@
 
     function changeSpeed(videoElement, speed) {
         videoElement.playbackRate = speed;
+    }
+
+    function createImageElement(imageName, altText) {
+        const image = document.createElement('img');
+        image.className = imageName;
+        image.src = chrome.runtime.getURL(imageName + '.png');
+        image.alt = altText;
+        image.width = 25;
+        image.height = 25;
+        return image;
     }
 })();
